1ec9ed3734939fca998553806af17ecba1babd1f
Hasnain Saleem <99191648+HasnainHSA@users.noreply.github.com>
2022-11-06 08:00:04 -0800

    Delete src directory

--- src/app/Store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  counterSlice  from './Counterslices'
-
-// redux toolkit store
-export const store = configureStore({
-  reducer: { counterSlice },
-})
